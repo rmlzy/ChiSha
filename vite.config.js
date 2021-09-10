89f5afb14cda49f2623a150ab758bbaa46b48345
rmlzy <rmlzy@outlook.com>
2021-09-10 19:11:20 +0800

    feat: test github action

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ChiSha/',
-  publicDir: process.env.NODE_ENV === 'production' ? '/ChiSha/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/ChiSha/' : '/',
   plugins: [vue()],
 });
