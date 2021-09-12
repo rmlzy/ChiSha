ec75acad34819ba843d40c5e5c0b6670d47e2c5d
rmlzy <rmlzy@outlook.com>
2021-09-12 10:59:44 +0800

    feat: test vite-plugin-inspect

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import Inspect from 'vite-plugin-inspect';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/ChiSha/' : '/',
-  plugins: [vue()],
+  plugins: [vue(), Inspect()],
 });
